61c47fe8a2062fd871bbc85799710a2c2bbaad76
Paul <35850997+CFtriksX@users.noreply.github.com>
2022-03-25 11:40:54 +0100

    Fetch plan from the api if not in localstore

--- src/plugins/autosave.js
+++ src/plugins/autosave.js
@@ -19,6 +19,15 @@ export default function autosave(autosaveKey, delay) {
       let data = localStorage.getItem(autosaveKey);
       let json = JSON.parse(data);
       store.dispatch(loadProject(json));
+    } else {
+      // If the map does not exist in the local storage it cleans the local storage and
+      // try to fecth it from the database. (Do nothing if it does not exist)
+      localStorage.clear();
+      fetch('/api/map?email=' + autosaveKey.slice(14))
+      .then(response => response.json())
+      .then((data) => {
+        if (data.success == true ) {store.dispatch(loadProject(data.saved_map));}
+      });
     }
 
     //update
