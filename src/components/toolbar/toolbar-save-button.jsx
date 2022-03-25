6e970c52ccd78c527c94e245b033f69fd0ee81b5
Paul <35850997+CFtriksX@users.noreply.github.com>
2022-03-25 11:35:50 +0100

    Remove browser save. Save with fetch and the api

--- src/components/toolbar/toolbar-save-button.jsx
+++ src/components/toolbar/toolbar-save-button.jsx
@@ -5,12 +5,26 @@ import ToolbarButton from './toolbar-button';
 import {browserDownload}  from '../../utils/browser';
 import { Project } from '../../class/export';
 
-export default function ToolbarSaveButton({state}, {translator}) {
+// Add mail and load pages in properties.
+export default function ToolbarSaveButton({state, mail, loadPages}, {translator}) {
 
+  // Instead of saving a file,
+  // it uses fetch to make an api call with the mail and the map.
+  // This might need an error handling if success != True
   let saveProjectToFile = e => {
     e.preventDefault();
     state = Project.unselectAll( state ).updatedState;
-    browserDownload(state.get('scene').toJS());
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'email': mail },
+      body: JSON.stringify(state.get('scene'))
+    };
+    fetch('/api/map', requestOptions)
+      .then(response => response.json());
+    
+    // Change page when the map is saved
+    loadPages();
+    // browserDownload(state.get('scene').toJS());
   };
 
   return (
